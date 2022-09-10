5f82285d9013cfe3fb610699c318a2d40c142451
design-system-dev <mysterioesermann@yahoo.de>
2022-09-10 14:33:20 +0200

    feat: generate design tokens

---
+++ packages/design-tokens/build.js
@@ -0,0 +1,42 @@
+const StyleDictionary = require('style-dictionary');
+const { join } = require('node:path');
+
+/** Classify all "space" and "size" tokens as "size", to ensure that they are being picked up by size transformations: */
+StyleDictionary.registerTransform({
+    name: 'custom/attribute/size',
+    type: 'attribute',
+    matcher: token =>
+        ['size', 'space'].some(category => token.path.includes(category)),
+    transformer(token) {
+        return { ...token.attributes, category: 'size' };
+    },
+});
+
+const config = {
+    source: [join(__dirname, 'tokens.json')],
+    platforms: {
+        styles: {
+            transforms: [
+                'attribute/cti',
+                'custom/attribute/size',
+                'name/cti/kebab',
+                'color/hex',
+                'size/pxToRem',
+            ],
+            files: [
+                {
+                    destination: join(__dirname, 'dist/tokens.css'),
+                    format: 'css/variables',
+                    options: {
+                        outputReferences: true,
+                        showFileHeader: false,
+                        selector: ':root, ::before, ::after, ::marker',
+                    },
+                },
+            ],
+        },
+    },
+};
+
+const sd = StyleDictionary.extend(config);
+sd.buildAllPlatforms();
