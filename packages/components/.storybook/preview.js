3b50b684662d81b6b2a90ebd10471a292a5c4bb9
design-system-dev <mysterioesermann@yahoo.de>
2022-09-12 07:00:12 +0200

    feat(storybook): add theme parameter

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -1,6 +1,27 @@
 import { defineCustomElements } from '../loader';
+import { html } from 'lit-html';
+import { styleMap } from 'lit-html/directives/style-map';
 
 import '../../design-tokens/dist/tokens.css';
 
 // load web components
 defineCustomElements();
+
+export const parameters = {
+    theme: 'light',
+};
+
+const paddingDecorator = story =>
+    html`<div style="padding: 1rem;">${story()}</div>`;
+
+const themeDecorator = (story, context) => html` <div
+    style=${styleMap({
+        background:
+            context?.parameters?.theme === 'dark' ? 'black' : 'transparent',
+        minHeight: '100vh',
+    })}
+>
+    ${story()}
+</div>`;
+
+export const decorators = [paddingDecorator, themeDecorator];
